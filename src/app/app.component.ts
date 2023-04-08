a7c2aba8dceb17cafb2f3fbc3b53fc0fa7ffbc72
Tech Wolf <77476375+TechWolf-38698@users.noreply.github.com>
2023-04-08 12:50:14 +0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   file1: File | null = null;
   file2: File | null = null;
+  error1: boolean = false;
+  error2: boolean = false;
+  error3: boolean = false;
+  loading: boolean = false;
   similarityScore: number | null = null;
 
   constructor(private http: HttpClient) {}
@@ -18,25 +22,39 @@ export class AppComponent {
     if (fileList.length > 0) {
       if (index === 1) {
         this.file1 = fileList[0];
+        this.error1 = false;
       } else {
         this.file2 = fileList[0];
+        this.error2 = false;
       }
     }
   }
 
   onCompare() {
+    this.error3 = false;
     if (this.file1 && this.file2) {
+      this.loading = true;
       const formData: FormData = new FormData();
       formData.append('file1', this.file1);
       formData.append('file2', this.file2);
-      this.http.post<any>('http://localhost:5000/compare-audio', formData).subscribe(
-        (response:any) => {
-          this.similarityScore = response.similarity_score;
-        },
-        (error:any) => {
-          console.log(error);
-        }
-      );
+      this.http
+        .post<any>('http://localhost:5000/compare-audio', formData)
+        .subscribe({
+          next: (response: any) => {
+            this.loading = false;
+            this.similarityScore = response.similarity_score;
+          },
+          error: (error: any) => {
+            this.loading = false;
+            this.error3 = true;
+          },
+        });
+    }
+    if (!this.file1) {
+      this.error1 = true;
+    }
+    if (!this.file2) {
+      this.error2 = true;
     }
   }
 }
