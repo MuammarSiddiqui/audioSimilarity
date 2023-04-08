2e50709b19decb53a77dab9ce6cee7d77e2ea1da
Muammar Siddiuqi <87115874+MuammarSiddiqui@users.noreply.github.com>
2023-04-08 11:25:42 +0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'audioSimilarity';
+  file1: File | null = null;
+  file2: File | null = null;
+  similarityScore: number | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  onFileChange(index: number, event: any) {
+    const fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      if (index === 1) {
+        this.file1 = fileList[0];
+      } else {
+        this.file2 = fileList[0];
+      }
+    }
+  }
+
+  onCompare() {
+    if (this.file1 && this.file2) {
+      const formData: FormData = new FormData();
+      formData.append('file1', this.file1);
+      formData.append('file2', this.file2);
+      this.http.post<any>('http://localhost:5000/compare-audio', formData).subscribe(
+        (response:any) => {
+          this.similarityScore = response.similarity_score;
+        },
+        (error:any) => {
+          console.log(error);
+        }
+      );
+    }
+  }
 }
